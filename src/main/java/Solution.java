a06fb2647c5d2f3e543e6e9ed24bc890bc3e694f
alek <wait82985@gmail.com>
2023-07-29 13:57:36 +0600

    initial commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        //TODO
+        return null;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class SolutionTest {
+    @Test
+    public void findRedundantConnectionTest1() {
+        int[][] edges = {{1, 2}, {1, 3}, {2, 3}};
+        int[] expected = {2, 3};
+        int[] actual = new Solution().findRedundantConnection(edges);
+        boolean result = Arrays.equals(expected, actual);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void findRedundantConnectionTest2() {
+        int[][] edges = {{1, 2}, {2, 3}, {3, 4}, {1, 4}, {1, 5}};
+        int[] expected = {1, 4};
+        int[] actual = new Solution().findRedundantConnection(edges);
+        boolean result = Arrays.equals(expected, actual);
+
+        Assert.assertTrue(result);
+    }
+}
