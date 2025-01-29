9e32ac10d44cafecd61e6f7a6f769e69ce9a6b35
alek <wait82985@gmail.com>
2025-01-29 18:37:24 +0500

    my own solution now

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,80 +1,68 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class Solution {
+class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        UnionFind uf = new UnionFind(edges.length);
-        int indexOfLatestRedundantEdge = -1;
-        for (int i = 0; i < edges.length; i++) {
-            if (!uf.unify(edges[i][0] - 1, edges[i][1] - 1)) {
-                indexOfLatestRedundantEdge = i;
+        Map<Integer, List<Integer>> g = new HashMap<>();
+        for (int[] e : edges) {
+            List<Integer> l = g.getOrDefault(e[0], new ArrayList<>());
+            l.add(e[1]);
+            g.put(e[0], l);
+            l = g.getOrDefault(e[1], new ArrayList<>());
+            l.add(e[0]);
+            g.put(e[1], l);
+        }
+        int n = g.size();
+        int[] vis = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (!ts(g, vis, i, 0, new int[] { 0, 0 })) {
+                break;
+            }
+        }
+        //System.out.println(Arrays.toString(vis));
+        // System.out.println(g);
+        for (int i = edges.length - 1; i >= 0; i--) {
+            if (vis[edges[i][0]] == 1 && vis[edges[i][1]] == 1) {
+                boolean noCycle = true;
+                int[] vis1 = new int[n + 1];
+                for (int j = 1; j <= n; j++) {
+                    if (!ts(g, vis1, j, 0, edges[i])) {
+                        noCycle = false;
+                        break;
+                    }
+                    
+                }
+                if(noCycle) {
+                    return edges[i];
+                }
+
+            }
+        }
+        return null;
+    }
+
+    boolean ts(Map<Integer, List<Integer>> g, int[] vis, int i, int prev, int[] removed) {
+        if (vis[i] == 2) {
+            return true;
+        }
+        if (vis[i] == 1) {
+            return false;
+        }
+        vis[i] = 1;
+
+        List<Integer> nei = g.get(i);
+        for (int ne : nei) {
+            if ((i == removed[0] && ne == removed[1]) || ((i == removed[1] && ne == removed[0]))) {
+                continue;
+            }
+            if (ne != prev) {
+                if (!ts(g, vis, ne, i, removed)) {
+                    return false;
+                }
             }
         }
 
-        return edges[indexOfLatestRedundantEdge];
-    }
-
-
-}
-
-class UnionFind {
-    int edges;
-    int[] parent;
-    int[] sizes;
-    int numberOfElements;
-
-    public UnionFind(int edges) {
-        this.edges = edges;
-        parent = new int[edges];
-        for (int i = 0; i < parent.length; i++) {
-            parent[i] = i;
-        }
-        sizes = new int[edges];
-        Arrays.fill(sizes, 1);
-        numberOfElements = edges;
-
-    }
-
-    public int getNumberOfElements() {
-        return numberOfElements;
-    }
-
-    public boolean isConnected(int node1, int node2) {
-        return find(node1) == find(node2);
-    }
-
-    public int find(int p) {
-
-        int root = p;
-        while (root != parent[root]) root = parent[root];
-
-        while (p != root) {
-            int next = parent[p];
-            parent[p] = root;
-            p = next;
-        }
-
-        return root;
-    }
-
-    public boolean unify(int node1, int node2) {
-        if (isConnected(node1, node2)) {
-            return false;
-        }
-
-        int parent1 = find(node1);
-        int parent2 = find(node2);
-
-        if (sizes[parent1] < sizes[parent2]) {
-            sizes[parent2] += sizes[parent2];
-            parent[parent1] = parent2;
-            sizes[parent1] = 0;
-        } else {
-            sizes[parent1] += sizes[parent2];
-            parent[parent2] = parent1;
-            sizes[parent2] = 0;
-        }
-        numberOfElements--;
+        vis[i] = 2;
         return true;
-    }
 
+    }
 }
